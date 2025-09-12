c309bb222550c21421354004f93132da28194b06
Aman Prajpati <aman.prajapati@eudemonic.co.in>
2025-09-12 23:26:55 +0530

    Footer

---
+++ src/app/HeaderFooter/Component/header.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: '../Template/header.component.html',
+  styleUrl: '../Style/header.component.css'
+})
+export class HeaderComponent {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './HeaderFooter/Component/header.component';
+import { FooterComponent } from './HeaderFooter/Component/footer.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
